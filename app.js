2af4edcbafb32c786689d9c6d7e5fa573f6524a5
rafaelburgueno <rafaelburg@gmail.com>
2022-01-17 09:04:42 -0300

    preparado para subir a heroku

--- app.js
+++ app.js
@@ -6,8 +6,9 @@
   const PORT = process.env.PORT || 3000;
 
 */
+require('dotenv').config()
 
-const port =  3000;
+const port =  process.env.PORT;
 
 const express = require("express")
 const path = require("path")
